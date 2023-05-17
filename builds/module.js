ce330ceaa3f8964b254f728fe9348896f01f1ee0
nickleibbrandt <nick-l@hotmail.de>
2023-05-17 10:24:26 +0200

    slider component

--- builds/module.js
+++ builds/module.js
@@ -1,11 +1,13 @@
 import floating from "../resources/js/plugins/floating.js";
 import popover from "../resources/js/plugins/popover.js";
 import tooltip from "../resources/js/plugins/tooltip.js";
+import slider from "../resources/js/plugins/slider.js";
 import toggle from "../resources/js/plugins/toggle.js";
 
 export {
     floating,
     popover,
     tooltip,
+    slider,
     toggle
 }
---
+++ resources/views/components/slider/thumb.blade.php
@@ -0,0 +1,3 @@
+<div x-slider:thumb
+     {{$attributes->class('absolute w-6 h-6 top-0 z-10 left-0 bg-white shadow-md border-2 border-primary rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2 transition focus:outline-none focus:ring-4 focus:ring-primary/20')}}
+></div>
